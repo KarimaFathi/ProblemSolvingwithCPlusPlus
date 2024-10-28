5dc171cb2ecce3b058c73683e1c1e74c01f10268
KarimaFathi <you@example.com>
2024-10-28 14:41:05 +0100

    Add Exo1

---
+++ Exo1/Exo1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void printName(string name) {   
+    cout << "\nYour full name is " << name << endl;
+}
+
+int main()
+{
+    printName("Karima");
+    return 0;
+}
+
