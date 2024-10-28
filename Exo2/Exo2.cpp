3c990ba17dead95c1a6afeee0488fcde77bda852
KarimaFathi <you@example.com>
2024-10-28 15:34:01 +0100

    Add Exo2

---
+++ Exo2/Exo2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+string readName() {
+	string name;
+	cout << "Please enter your name.\n";
+	getline(cin, name);
+	return name;
+}
+void printName(string name) {
+	cout << "Your name is " << name << endl;
+}
+
+
+
+int main()
+{
+
+	printName(readName());
+}
+
