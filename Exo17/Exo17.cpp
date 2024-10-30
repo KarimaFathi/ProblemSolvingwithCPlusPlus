669d1b9b6de6fdc94f0c133a8b18c8458b4543b3
KarimaFathi <you@example.com>
2024-10-30 13:01:33 +0100

    add Exo17

---
+++ Exo17/Exo17.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void readDimensions(float& a, float& h) {
+    cout << "Please enter the rectangle's base.\n";
+    cin >> a;
+    cout << "Please enter the rectangle's height.\n";
+    cin >> h;
+}
+
+float calculateArea(int a, int h) {
+    return (float) a / 2 * h ;
+}
+
+void printArea(float area) {
+    cout << "The rectangle area is " << area << endl;
+}
+
+
+int main()
+{
+    float a, h;
+    readDimensions(a, h);
+    printArea(calculateArea(a, h));
+}
