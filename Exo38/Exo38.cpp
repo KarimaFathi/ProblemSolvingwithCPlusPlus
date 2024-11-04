2cb7a8c5b9101fac42415471bf18e8ea57df1ead
KarimaFathi <you@example.com>
2024-11-04 12:11:02 +0100

    add Exo38

---
+++ Exo38/Exo38.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+// we can use enum here too as another option;
+int readPositiveNumber() {
+    int number;
+    do {
+        cout << "Please enter a positive number.\n";
+        cin >> number;
+    } while (number < 0);
+    return number;
+}
+
+
+string isPrime(int num) {
+
+    for (int i = 2; i <= round(num / 2); i++) {
+        if (num % i == 0) {
+            return "not Prime";
+        }
+    }
+    return "Prime";
+}
+
+int main()
+{
+    cout << "This number is " << isPrime(readPositiveNumber());
+    return 0;
+}
